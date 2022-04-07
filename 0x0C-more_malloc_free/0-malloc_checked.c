68ddb6f180b1386add610dbce93d103d81ecd1f3
yoftahe351 <yoftaheberhan@gmail.com>
2022-04-07 12:43:28 -0700

    mandatory tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * malloc_checked - allocates memory.
- * @b: amount of bytes.
- *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * malloc_checked - Entry point
+ *@b: string we need to duplicate
+ * Return: a pointer to the allocate memory
  */
 void *malloc_checked(unsigned int b)
 {
-char *p;
-p = malloc(b);
-if (p == NULL)exit(98);
-return (p);
+	void *p = NULL;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
 }
