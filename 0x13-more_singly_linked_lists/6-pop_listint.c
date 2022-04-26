e2b877a87d4450fceb41c2a81a6fc3e5703411c7
yoftahe351 <yoftaheberhan@gmail.com>
2022-04-26 14:30:50 -0700

    mandatory task

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,30 +1,23 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * pop_listint - deletes the head node of
- * a linked list
- * @head: head of a list.
- *
- * Return: head node's data.
- */
+ * pop_listint - Delete the first element of a singly linked list.
+ * @head: Pointer to a list.
+ * Return: Integer if success.
+ **/
+
 int pop_listint(listint_t **head)
 {
-  int hnode;
-  listint_t *h;
-  listint_t *curr;
+	listint_t *tp;
+	int my_data;
 
-  if (*head == NULL)
-    return (0);
+	if (*head == NULL)
+		return (0);
 
-  curr = *head;
-
-  hnode = curr->n;
-
-  h = curr->next;
-
-  free(curr);
-
-  *head = h;
-
-  return (hnode);
+	tp = *head;
+	*head = tp->next;
+	my_data = tp->n;
+	free(tp);
+	return (my_data);
 }
